8c0fbe01cf17db29af54928ff3da414f04f50f97
Alex Kim <alexkim251@yahoo.com>
2021-05-17 16:50:15 -0500

    first few steps

--- client.js
+++ client.js
@@ -1,1 +1,30 @@
 console.log('Here are all the available people:', people);
+
+$(document).ready(readyNow);
+
+function readyNow() {
+    console.log('jquery ready');
+
+    displayImages();
+    randomPerson();
+}
+
+function displayImages() {
+    for (let i = 0; i < people.length; i++) {
+        let portrait = $(`<img src="https://github.com/${people[i].githubUsername}.png?size=250" alt="Profile image of ${people[i].name}"></img>`)
+        portrait.data('portraitName',people[i].name)
+        console.log($(portrait).data());
+        $('#imagesBody').append(portrait);
+    }
+    }
+
+    function randomNumber(min, max){
+        return Math.floor(Math.random() * (1 + max - min) + min);
+    }
+    
+
+function randomPerson() {
+
+    $('#prompt').append(people[randomNumber(0,people.length-1)].name);
+
+}
