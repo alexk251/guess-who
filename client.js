adca7c95062d4fffac146f98d6639689a6e348f4
Alex Kim <alexkim251@yahoo.com>
2021-05-17 17:35:44 -0500

    some more done

--- client.js
+++ client.js
@@ -2,29 +2,52 @@ console.log('Here are all the available people:', people);
 
 $(document).ready(readyNow);
 
+let currentRandomPerson = ( people[randomNumber(0,people.length-1)].name );
+
 function readyNow() {
     console.log('jquery ready');
 
     displayImages();
     randomPerson();
+
+    $('#imagesBody').on('click', '.image' ,gameLogic );
+    
+}
+
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
 }
 
 function displayImages() {
     for (let i = 0; i < people.length; i++) {
-        let portrait = $(`<img src="https://github.com/${people[i].githubUsername}.png?size=250" alt="Profile image of ${people[i].name}"></img>`)
+        let portrait = $(`<img class="image" src="https://github.com/${people[i].githubUsername}.png?size=250" alt="Profile image of ${people[i].name}"></img>`)
         portrait.data('portraitName',people[i].name)
         console.log($(portrait).data());
         $('#imagesBody').append(portrait);
     }
-    }
+}
 
-    function randomNumber(min, max){
-        return Math.floor(Math.random() * (1 + max - min) + min);
-    }
     
 
 function randomPerson() {
 
-    $('#prompt').append(people[randomNumber(0,people.length-1)].name);
+    $('#prompt').addClass('rando').append(currentRandomPerson);
+    
+
+}
+
+function gameLogic() {
+    let selectedImage = $(this).data('portraitName');
+    console.log('click');
+
+    console.log(selectedImage);
+
+    if (selectedImage == currentRandomPerson) {
+        alert('we have a winner')
+    } else {
+        alert('try again');
+    }
+
+
 
 }
