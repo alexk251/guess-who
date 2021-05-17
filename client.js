8af72e0a25c6a7e911008bee14e504055fc302f2
Alex Kim <alexkim251@yahoo.com>
2021-05-17 18:09:08 -0500

    well i broke it while refactoring it

--- client.js
+++ client.js
@@ -2,13 +2,12 @@ console.log('Here are all the available people:', people);
 
 $(document).ready(readyNow);
 
-let currentRandomPerson = ( people[randomNumber(0,people.length-1)].name );
-
 function readyNow() {
     console.log('jquery ready');
 
     displayImages();
     randomPerson();
+    
 
     $('#imagesBody').on('click', '.image' ,gameLogic );
     
@@ -31,18 +30,28 @@ function displayImages() {
 
 function randomPerson() {
 
-    $('#prompt').addClass('rando').append(currentRandomPerson);
+    $('#prompt').text('');
+
+    let justAPerson =  people[randomNumber(0,people.length-1)].name;
+
+    let currentRandomPerson = $(`<p>Click On: ${justAPerson}</p>`);
+
+    currentRandomPerson.data('rando',justAPerson);
+    $('#prompt').append(currentRandomPerson);
+
+    
     
 
 }
 
 function gameLogic() {
     let selectedImage = $(this).data('portraitName');
+    let findThisPerson = $('#prompt').data('rando');
     console.log('click');
-
+    console.log(findThisPerson)
     console.log(selectedImage);
 
-    if (selectedImage == currentRandomPerson) {
+    if (selectedImage === findThisPerson) {
         alert('we have a winner')
     } else {
         alert('try again');
